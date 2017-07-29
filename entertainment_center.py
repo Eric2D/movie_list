add9bb85f319ba05996e5420ffa7f13803f6d613
Eric Davis <ericdd2221@gmail.com>
2017-07-29 01:01:13 -0500

    Cleaned up conventional errors of entertainment_center.py and media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,10 @@
-#use the file here to make your favorite movie lists through media
+# Use the file here to make your favorite movie lists through media
 
 import media
 import fresh_tomatoes
 
 
-oblivion = media.movie('Oblivion','https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwMDY0MTA4MF5BMl5BanBnXkFtZTcwNzI3MDgxOQ@@._V1_UY1200_CR64,0,630,1200_AL_.jpg','https://www.youtube.com/watch?v=XmIIgE7eSak')
+oblivion = media.movie('Oblivion', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwMDY0MTA4MF5BMl5BanBnXkFtZTcwNzI3MDgxOQ@@._V1_UY1200_CR64,0,630,1200_AL_.jpg', 'https://www.youtube.com/watch?v=XmIIgE7eSak')
 
 pacific_rim = media.movie('Pacific Rim: Uprising', 'https://resizing.flixster.com/MbiJhGRyRcftBfFpMg61V2OuEdQ=/300x300/v1.bTsxMTE3MzkzMztqOzE3NDU0OzEyMDA7ODAwOzEyMDA', 'https://www.youtube.com/watch?v=5guMumPFBag')
 
--- media.py
+++ media.py
@@ -1,5 +1,5 @@
 class movie():
-    #connects movie information to a variable
+    # Connects movie information to a variable
     def __init__(self, title, poster_url, youtube_url):
         self.title = title
         self.poster_image_url = poster_url
