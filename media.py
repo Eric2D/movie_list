f620335c93531669e386c1a7eee4a35eded7e381
Eric Davis <ericdd2221@gmail.com>
2017-07-30 02:02:32 -0500

    added more comments to explain the class movie

--- media.py
+++ media.py
@@ -1,5 +1,10 @@
 class movie():
     # Connects movie information to a variable
+    # The title of the movie is the first input.
+    # The second is the poster url image for the movie.
+    # The third is the youtube url for the trailer.
+    # These variables will be called in fresh_tomatoes
+    # to post said information to the web page.
     def __init__(self, title, poster_url, youtube_url):
         self.title = title
         self.poster_image_url = poster_url
